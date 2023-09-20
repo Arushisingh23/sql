2e85c3a25abf249805bf80ee5d3c64e9ff674347
Arushi singh <85683967+Arushisingh23@users.noreply.github.com>
2023-09-21 04:33:53 +0530

    Update Lques11.sql

--- Lques11.sql
+++ Lques11.sql
@@ -62,7 +62,10 @@ Output:
 | 100        | 2008       | 10       | 5000  |
 | 200        | 2011       | 15       | 9000  |
 +------------+------------+----------+-------+*/
-select product_id, year as first_year, quantity, price
-from sales
-group by product_id
-having year=min(year)
+SELECT product_id, year as first_year, quantity,price
+FROM Sales
+WHERE (product_id,year) in (
+SELECT product_id,MIN(year)
+FROM Sales
+GROUP BY product_id
+)
